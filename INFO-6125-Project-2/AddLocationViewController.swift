fc5eec462957c34a99ba903ce5592b7c3e370b1b
rajdeepf1 <rajdeepf1@gmail.com>
2022-11-29 20:35:18 -0500

    Initial Commit

---
+++ INFO-6125-Project-2/AddLocationViewController.swift
@@ -0,0 +1,32 @@
+//
+//  AddLocationViewController.swift
+//  INFO-6125-Project-2
+//
+//  Created by Rajdeep Singh on 2022-11-29.
+//
+
+import UIKit
+
+class AddLocationViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func goBackButtonPressed(_ sender: Any) {
+        dismiss(animated: true)
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- INFO-6125-Project-2/ViewController.swift
+++ INFO-6125-Project-2/ViewController.swift
@@ -8,12 +8,26 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    
+    
+    @IBAction func onAddLocationButtonPressed(_ sender: Any) {
+        
+        print("hi")
+        
+        performSegue(withIdentifier: "goToAddLocation", sender: self)
+        
+    }
+    
+    
+    
 }
+
+
+
 
