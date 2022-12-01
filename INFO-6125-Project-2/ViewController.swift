c1b3c5da751b9186e0c4c683e4aa18ed602f0d86
rajdeepf1 <rajdeepf1@gmail.com>
2022-11-30 23:32:16 -0500

    annotations implemented basic

--- INFO-6125-Project-2/ViewController.swift
+++ INFO-6125-Project-2/ViewController.swift
@@ -6,13 +6,47 @@
 //
 
 import UIKit
+import MapKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var mapView:MKMapView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        setupMap()
+        addAnnotation(location: getCurrentLocation())
     }
+    
+    
+    private func setupMap(){
+            let location = getCurrentLocation()
+            let radiusInMeters: CLLocationDistance = 1000
+            let region = MKCoordinateRegion(center: location.coordinate,
+                                            latitudinalMeters: radiusInMeters,
+                                            longitudinalMeters: radiusInMeters)
+            mapView.setRegion (region, animated: true)
+            
+            // camera boundaries
+            let cameraBoundary = MKMapView.CameraBoundary(coordinateRegion: region)
+            mapView.setCameraBoundary(cameraBoundary, animated: true)
+            // control zooming
+            let zoomRange = MKMapView.CameraZoomRange(maxCenterCoordinateDistance: 100000)
+            mapView.setCameraZoomRange(zoomRange, animated: true)
+            
+        }
+    
+    private func addAnnotation(location: CLLocation) {
+          let annotation = MyAnnotation(coordinate: location.coordinate, title: "You are here!", subtitle:"your current location.")
+          mapView.addAnnotation (annotation)
+      }
+    
+    private func getCurrentLocation() -> CLLocation{
+            return CLLocation(latitude: 43.010449, longitude: -81.1975784)
+        }
+    
+    
     
     
     
@@ -25,6 +59,19 @@ class ViewController: UIViewController {
     }
     
     
+    
+}
+
+class MyAnnotation: NSObject, MKAnnotation{
+    var coordinate: CLLocationCoordinate2D
+    var title: String?
+    var subtitle: String?
+    init (coordinate: CLLocationCoordinate2D, title: String, subtitle: String) {
+    self.coordinate = coordinate
+    self.title = title
+    self.subtitle = subtitle
+        super.init()
+    }
     
 }
 
